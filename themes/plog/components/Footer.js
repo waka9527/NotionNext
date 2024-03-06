fbdc26509f35cf38d7bc4a6057a7b35eca9871f5
waka9527 <162448675+waka9527@users.noreply.github.com>
2024-03-06 20:16:02 +0800

    Update Footer.js

--- themes/plog/components/Footer.js
+++ themes/plog/components/Footer.js
@@ -1,20 +1,11 @@
-import Vercel from '@/components/Vercel'
-import { siteConfig } from '@/lib/config'
+import React from 'react';
 
-export const Footer = (props) => {
-  const d = new Date()
-  const currentYear = d.getFullYear()
-
-  const since = siteConfig('SINCE')
-  const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
-  return <footer className={'z-10 relative mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all' } >
-     <div className="my-4 text-sm leading-6">
-       <div className="flex align-baseline justify-start flex-wrap space-x-6">
-         <div> © {siteConfig('AUTHOR')} {copyrightDate}  </div>
-         <div>Powered By <a href="https://github.com/tangly1024/NotionNext" className='underline'>NotionNext {siteConfig('VERSION')}</a></div>
-         <Vercel />
-       </div>
-     </div>
-   </footer>
+const Footer = () => {
+  return (
+    <footer>
+      {/* 这里是空的 */}
+    </footer>
+  );
 }
+
+export default Footer;
